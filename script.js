ff1b36085c1afb55b5753804cab67bac5c6c6a94
ukinon <arfianoj@gmail.com>
2022-07-06 17:57:14 +0700

    newwww

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const totalharga = document.getElementById("total");
 const customer = document.getElementById("customer");
 
 
-function changeHarga(){
+function changeHarga50(){
   const hargaPerOrang = 50000;
   customer.addEventListener("input", function(ev){
     const value = ev.currentTarget.value;
@@ -11,15 +11,52 @@ function changeHarga(){
     totalharga.value = total.toFixed(2);
     });
   };
+  function changeHarga40(){
+    const hargaPerOrang = 40000;
+    customer.addEventListener("input", function(ev){
+      const value = ev.currentTarget.value;
+      console.log(value);
+      const total = value * hargaPerOrang;
+      totalharga.value = total.toFixed(2);
+      });
+    };
+    function changeHarga30(){
+      const hargaPerOrang = 30000;
+      customer.addEventListener("input", function(ev){
+        const value = ev.currentTarget.value;
+        console.log(value);
+        const total = value * hargaPerOrang;
+        totalharga.value = total.toFixed(2);
+        });
+      };
+      function changeHarga100(){
+        const hargaPerOrang = 100000;
+        customer.addEventListener("input", function(ev){
+          const value = ev.currentTarget.value;
+          console.log(value);
+          const total = value * hargaPerOrang;
+          totalharga.value = total.toFixed(2);
+          });
+        };
+        function changeHarga150(){
+          const hargaPerOrang = 150000;
+          customer.addEventListener("input", function(ev){
+            const value = ev.currentTarget.value;;
+            console.log(value);
+            const total = value * hargaPerOrang;
+            totalharga.value = total.toFixed(2);
+            });
+          };
 
     $(document).ready(function (){
         $("#backbtn").click(function () {
           $("#popup").fadeOut();
+          form.reset();
         });     
       });
       $(document).ready(function (){
         $("#thankyou").click(function () {
-          $("#thankyou").fadeOut();
+          $("#thankyou").fadeOut(); 
         });     
       });
 
